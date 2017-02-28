840d1b824292bc06f4398d3f2a165d3fb70b0171
anete.anetes <anete.anetes@gmail.com>
2017-02-28 12:46:37 +0500

    communication core

---
+++ geranium/backend/Request.ts
@@ -0,0 +1,23 @@
+﻿module geranium.backend {
+    export abstract class Request {
+        protected communicator: interfaces.ICommunicator;
+
+        constructor(communicator: interfaces.ICommunicator) {
+            this.communicator = communicator;
+        }
+
+        send<TResponse>(data: any): PromiseLike<TResponse> {
+            return new Promise((resolve, reject) => {
+                try {
+                    resolve(this.communicator.send<any>(data));
+                }
+                catch (ex) { reject(null); }
+            })
+                .then<TResponse>(x => this.communicator.recive<TResponse>())
+                .catch(this.catchPromise);
+        }
+
+        protected abstract catchPromise(err: any);
+        protected abstract resolvePromise();
+    }
+}
