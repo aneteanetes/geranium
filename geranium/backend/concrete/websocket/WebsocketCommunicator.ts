c22db7b2fca4ce9e13c50b938bcd1a2639c77c38
anetegithub <anete.anetes@gmail.com>
2017-02-28 23:51:43 +0500

    websockets-communication added

--- geranium/backend/concrete/websocket/WebsocketCommunicator.ts
+++ geranium/backend/concrete/websocket/WebsocketCommunicator.ts
@@ -8,25 +8,44 @@
                 this.socket = new WebSocket(endpoint);
             else
                 this.socket = new MozWebSocket(endpoint);
-            this.socket.onmessage = x => {
-                return x.data;
+            this.socket.onmessage = (x) => {
+                this.data = x.data;
             };
         }
 
         send<TRequest>(data: TRequest) {
-            this.socket.send(data);
+            return this.socketOpened()
+                .then(() => { this.socket.send(data) })
+                .catch(this.catchSocketError);
         }
         recive<TResponse>(): PromiseLike<TResponse> {
-            return new Promise((resolve, reject) => {
-                try {
-                    resolve(this.socket.onmessage);
-                } catch (ex) {
-                    reject(ex);
-                }
-            }).catch(this.catchSocketError);
+            return this.socketRecived()
+                .then(x => {
+                    var _return = this.data;
+                    this.data = null;
+                    return _return;
+                })
+                .catch(this.catchSocketError);
         }
         catchSocketError(ex: Error) {
             console.log(ex);
+        }
+        private data: any;
+        socketRecived() {
+            return new Promise(function (resolve) {
+                setTimeout(resolve, 30);
+            }).then(() => {
+                if (this.data == null)
+                    this.socketRecived();
+            });
+        }
+        socketOpened() {
+            return new Promise(function (resolve) {
+                setTimeout(resolve, 30);
+            }).then(() => {
+                if (this.socket.readyState != this.socket.OPEN)
+                    this.socketOpened();
+            });
         }
     }
 }
--- geranium/geranium.ts
+++ geranium/geranium.ts
@@ -1,24 +1,5 @@
 ﻿module geranium {
     export async function blossom() {
-        var uri = 'ws://' + window.location.hostname + ':58004/handler.ashx'; 
-        var requester = new backend.WebSocketRequest(uri,onError);
-
-        var first: number = 5;
-        var opts = {
-            url: '/test.q',
-            method: 'POST',
-            data: { n: first }
-        };
-        var second = await requester.send<number>(opts);
-        opts.data.n = second;
-        var third = await requester.send<number>(opts);
-
-        console.log(first);
-        console.log(second);
-        console.log(third);
-    }
-    function onError(err: exceptions.Exception) {
-        console.error(err);
     }
 }
 geranium.blossom();
