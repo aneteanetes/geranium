ef69e10d281d0c1c11022b56f9eee8ae25cddb1c
anete.anetes <anete.anetes@gmail.com>
2017-03-01 16:47:09 +0500

    local storage added

---
+++ geranium/runtime/storage/concrete/LocalStorage.ts
@@ -0,0 +1,22 @@
+﻿module geranium.runtime {
+    export class LocalStorage implements storage.interfaces.IStorage {
+        add(model: any): boolean {
+            try {
+                localStorage.setItem(model.constructor.name, model.toString());
+                return true;
+            } catch (ex) {
+                return false;
+            }
+        }
+        remove<T>(type: { new (...args: any[]): T, name: string }): boolean {
+            try {
+                localStorage.removeItem(type.name);
+                return true;
+            } catch (ex) {
+                return false;
+            }
+        }
+        get<T>(type: { new (...args: any[]): T, name: string }): T { return localStorage.getItem(type.name); }
+
+    }
+}
