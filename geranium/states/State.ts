849a57454709f4da47f767bdec07b5cb7cfb5065
anetegithub <anete.anetes@gmail.com>
2017-03-01 21:21:24 +0500

    states added

---
+++ geranium/states/State.ts
@@ -0,0 +1,10 @@
+﻿module geranium.states {
+    export abstract class State {
+        constructor() {
+            runtime.AppSettings.Current.storage.add(this);
+        }
+        static get<T extends State>(type: { new (...args: any[]): T }): T {
+            return runtime.AppSettings.Current.storage.get(type);
+        }
+    }
+}
