afd41e7ad2e5e80e68e9cb312da52fa3495a249a
anete.anetes <anete.anetes@gmail.com>
2017-03-03 10:40:21 +0500

    state - model dependency

--- geranium/models/abstract/Model.ts
+++ geranium/models/abstract/Model.ts
@@ -1,4 +1,16 @@
 ﻿module geranium.models.abstract {
     export abstract class Model {
+
+        get refreshable() {
+            return (typeof this.autoupdate() !== 'boolean');
+        }
+        get params(): {} {
+            return this.autoupdate();
+        }
+        set obtain(data: any) {
+            Object.assign(this, JSON.parse(data));
+        }
+
+        protected abstract autoupdate(): boolean | {};
     }
 }
--- geranium/states/State.ts
+++ geranium/states/State.ts
@@ -1,6 +1,7 @@
 ﻿module geranium.states {
-    export abstract class State {
+    export abstract class State extends models.abstract.Model {
         constructor() {
+            super();
             runtime.AppSettings.Current.states.add(this);
         }
         static get<T extends State>(type: { new (...args: any[]): T }): T {
@@ -9,17 +10,5 @@
         remove(): boolean {
             return runtime.AppSettings.Current.states.remove((<any>this).constructor);
         }
-
-        get refreshable() {
-            return (typeof this.autoupdate() !== 'boolean');
-        }
-        get params(): {} {
-            return this.autoupdate();
-        }
-        set obtain(data: any) {
-            Object.assign(this, JSON.parse(data));
-        }
-
-        protected abstract autoupdate(): boolean | {};
     }
 }
