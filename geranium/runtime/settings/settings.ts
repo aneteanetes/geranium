dbf843439786c4e3ddb1936226de783ab28eaa14
anete.anetes <anete.anetes@gmail.com>
2017-03-03 12:33:46 +0500

    session extension

--- geranium/runtime/settings/settings.ts
+++ geranium/runtime/settings/settings.ts
@@ -1,10 +1,17 @@
 ﻿module geranium.runtime {
     export abstract class AppSettings {
-        private static _current: _AppSettings;
+        private settings_storage: storage.interfaces.IStorage;
+        constructor() {
+            this.settings_storage = new WindowStorage("geranium-settings");
+            this.settings_storage.add(this);
+            AppSettings._current = this;
+        }
+
+        private static _current: AppSettings;
         static get Current(): AppSettings {
             if (AppSettings._current == null)
-                AppSettings._current = new _AppSettings();
-            return AppSettings._current;
+                new _AppSettings();
+            return AppSettings._current;//.settings_storage.get(_AppSettings);
         }
 
         private static initialized: boolean = false;
@@ -24,7 +31,7 @@
         }
 
         logger: exceptions.logging.ILogger = new exceptions.ConsoleLogger();
-        request: backend.abstract.Request = new backend.AjaxRequest((x) => { console.log(x); });
+        request: backend.abstract.StatefullRequest = new backend.AjaxRequest((x) => { console.log(x); });
         templating: any = {};
         storage: storage.interfaces.IStorage = new WindowStorage("geranium-data-storage");
         states: storage.interfaces.IGenericStorage<states.State> = new StatesStorage("geranium-states-storage");
