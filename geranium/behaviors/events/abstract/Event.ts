43fc7a4ece92b607075dcb12ea610b326aa9257f
anete.anetes <anete.anetes@gmail.com>
2017-03-03 18:03:55 +0500

    event declaration added

---
+++ geranium/behaviors/events/abstract/Event.ts
@@ -0,0 +1,20 @@
+﻿module geranium.behaviors.events.abstract {
+    export abstract class Event<T>{
+
+        abstract execute(data: T);
+
+        protected trigger() {
+            this._requestEvents.forEach(x => {
+                this.execute(x);
+            });
+        }
+
+        private _requestEvents: T[];
+        set bind(callback: T) {
+            this._requestEvents.push(callback);
+        }
+        set unbind(callback: T) {
+            //this._requestEvents.remove(callback);
+        }
+    }
+}
