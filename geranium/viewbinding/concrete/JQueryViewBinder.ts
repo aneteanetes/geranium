7ac6d591f432ced03bf097cab0747d4a89680759
anete.anetes <anete.anetes@gmail.com>
2017-03-06 12:35:32 +0500

    symbol binding

--- geranium/viewbinding/concrete/JQueryViewBinder.ts
+++ geranium/viewbinding/concrete/JQueryViewBinder.ts
@@ -3,21 +3,23 @@
         private static DATAFIELD = '[data-field]';
 
         protected bindFields(ViewDOM: viewDOM.abstract.ViewDOM) {
-            debugger;
             var DOM = ViewDOM.getViewDOM<JQuery>();
             DOM.findAndfilter(JQueryViewBinder.DATAFIELD).each((i, e) => {
                 let $e = $(e);
                 var field = $e.data('field');
                 $e.removeAttr(JQueryViewBinder.DATAFIELD.replace('[', '').replace(']', ''));
-                ViewDOM.view.data["_" + field] = ViewDOM.view.data[field];
+
+                var fieldSymbol = Symbol(field);
+
+                ViewDOM.view.data[fieldSymbol] = ViewDOM.view.data[field];
                 Object.defineProperty(ViewDOM.view.data, field, {
-                    get: () => { return ViewDOM.view.data["_" + field]; },
-                    set: (val) => { $e.html(val); ViewDOM.view.data["_" + field] = val; }
+                    get: () => { return ViewDOM.view.data[fieldSymbol]; },
+                    set: (val) => { $e.html(val); ViewDOM.view.data[fieldSymbol] = val; }
                 });
             });
         }
         protected bindProperties(ViewDOM: viewDOM.abstract.ViewDOM) {
-
+           
         }
         protected bindMethods(ViewDOM: viewDOM.abstract.ViewDOM) {
 
