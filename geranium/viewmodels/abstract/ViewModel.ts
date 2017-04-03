5381b0a83c1eb4c80f5a302e9e028798866a5317
anetegithub <anete.anetes@gmail.com>
2017-04-03 13:14:34 +0500

    resolve #2

--- geranium/viewmodels/abstract/ViewModel.ts
+++ geranium/viewmodels/abstract/ViewModel.ts
@@ -1,34 +1,39 @@
 ﻿namespace geranium.viewmodels.abstract {
-    @routing.routeignore
-    export abstract class ViewModel extends models.abstract.Model implements view.interfaces.IViewed {
-        private publishedViewDom: viewDOM.abstract.ViewDOM;
-        protected get markup(): viewDOM.abstract.ViewDOM { return this.publishedViewDom; }
+	@routing.routeignore
+	export abstract class ViewModel extends models.abstract.Model implements view.interfaces.IViewed {
+		private publishedViewDom: viewDOM.abstract.ViewDOM;
+		protected get markup(): viewDOM.abstract.ViewDOM { return this.publishedViewDom; }
 
-        async display(selector: string) {
+		async display(selector: string) {
 
-            if (history.is(this.constructor) && !this['#routed']) {
-                var _routed = this["@routed"] as routing.contracts.RouteMatch;
-                _routed.selector = selector;
-                runtime.appSettings.router.route(_routed);
-            }
-            else
-                delete this['#routed'];
+			if (history.is(this.constructor) && !this['#routed']) {
+				var _routed = this["@routed"] as routing.contracts.RouteMatch;
+				_routed.selector = selector;
+				var _history = new history.contracts.HistoryItem();
+				_history.url = _routed.url;
+				_history.title = document.title;
+				_history.state = {
+					ctor: this.constructor.name,
+					selector: selector
+				};
+				runtime.appSettings.history.extend(_history);
+			}
+			delete this['#routed'];
 
-            var title = this.documentTitle();
-            if (title != null)
-                document.title = title;
+			var title = this.documentTitle();
+			if (title != null)
+				document.title = title;
+			this.publishedViewDom = await runtime.appSettings.viewengine.execute({
+				iViewed: this,
+				selector: selector
+			});
+		}
 
-            this.publishedViewDom = await runtime.appSettings.viewengine.execute({
-                iViewed: this,
-                selector: selector
-            });
-        }
+		documentTitle(): string { return null; }
+		abstract view(): { new (selector: string): view.abstract.View } | string;
 
-        documentTitle(): string { return null; }
-        abstract view(): { new (selector: string): view.abstract.View } | string;
-
-        async toString(selector) {
-            this.display(selector);
-        }
-    }
+		async toString(selector) {
+			this.display(selector);
+		}
+	}
 }
