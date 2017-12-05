2e47a00255f3a5762680f56e00575bd67484f711
anete.anetes <anete.anetes@outlook.com>
2017-12-05 14:25:41 +0300

    ajax communicator without jQuery

--- geranium/backend/concrete/ajax/AjaxCommunicator.ts
+++ geranium/backend/concrete/ajax/AjaxCommunicator.ts
@@ -1,17 +1,61 @@
 ﻿import { ICommunicator } from "../../interfaces/ICommunicator";
+import { CommunicationException } from "../../../exceptions/backend/CommunicationException";
 
 export class AjaxCommunicator extends ICommunicator {
-    private innerPromise: JQueryPromise<any>;
+    private innerPromise: Promise<any>;
 
-    send<TRequest extends JQueryAjaxSettings>(data: TRequest): Promise<void> {
+    send<TRequest extends XHRSettings>(data: TRequest): Promise<void> {
+        this.validate(data);
+
         return new Promise<void>((resolve, reject) => {
-            this.innerPromise = $.ajax(data);
+            this.innerPromise = new Promise((resolve, reject) => {
+                try {
+                    let xhr = new XMLHttpRequest();
+                    this.setContentType(data, xhr);
+                    xhr.open(data.method, data.url, data.async, data.user, data.pasw);
+
+                    xhr.onload = function () {
+                        if (xhr.status >= 200 && xhr.status < 400) {
+                            resolve(xhr.responseText);
+                        } else {
+                            reject(`${xhr.status}: ${xhr.statusText}`);
+                        }
+                    };
+
+                    xhr.onerror = function () {
+                        reject(`${xhr.status}: ${xhr.statusText}`);
+                    }
+                } catch (ex) {
+                    throw new CommunicationException(ex);
+                }
+            });
         })
     }
 
     async recive<TResponse>(): Promise<TResponse> {
-        //TODO : return await Promise vs return Promise;
-        //link : realization, not concept
         return this.innerPromise;
     }
+
+    private validate(data: XHRSettings) {
+        if (!data.method || !data.url) {
+            throw new CommunicationException("XHR request must declare method and url!");
+        }
+    }
+
+    private setContentType(opt: XHRSettings, xhr: XMLHttpRequest) {
+        if (opt.method != "GET") {
+            xhr.setRequestHeader("Content-Type", 'application/x-www-form-urlencoded; charset=UTF-8');
+        }
+    }
 }
+
+export interface XHRSettings {
+    method?: HttpMethod;
+    url?: string;
+    async?: boolean;
+    user?: string;
+    pasw?: string;
+    data?: any
+}
+
+type HttpMethod = keyof { GET, POST, PUT, PATCH, DELETE };
